53a91a8a690b48635d9ca7fc25b87945c673d195
Mustafo <aminboevmustafo5@gmail.com>
2026-05-06 17:08:42 +0500

    frist_commit

--- project/settings.py
+++ project/settings.py
@@ -116,9 +116,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+MEDIA_ROOT=BASE_DIR/'media/'
+MEDIA_URL='/media/'
 STATIC_URL = 'static/'
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# create funktion calkulyatot
